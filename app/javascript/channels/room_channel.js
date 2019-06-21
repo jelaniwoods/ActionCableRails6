16dda3be60b28f412c61103e97199ebc77ed3834
Jelani Woods <jelani@firstdraft.com>
2019-06-21 12:44:22 -0500

    Update index page with messages

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,6 +12,7 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    $('#msg').append('<div class="message"> ' + data.content + '</div>')
     console.log("Recieving:")
     console.log(data.content)
   }
