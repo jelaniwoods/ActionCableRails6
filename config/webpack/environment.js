c4603688d0d934e00375d1692c1625cf4e799dc3
Jelani Woods <jelani@firstdraft.com>
2019-06-21 11:18:08 -0500

    Require jquery in environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new WebPack.ProviderPlugin({
+    $: 'jquery/src/jsquery',
+    JQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
