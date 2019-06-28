9875ece67815e23d50800b19f7bba4027a5d34f4
Jelani Woods <jelani@firstdraft.com>
2019-06-28 12:39:07 -0500

    Revert "wip"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_173645) do
+ActiveRecord::Schema.define(version: 2019_06_28_173358) do
 
   create_table "contexts", force: :cascade do |t|
     t.string "title"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2019_06_28_173645) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
