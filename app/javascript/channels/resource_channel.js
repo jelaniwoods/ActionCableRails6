7c20c7016205c84f8593e5f57647a3fdf813fd99
Jelani Woods <jelani@firstdraft.com>
2019-06-28 17:00:13 -0500

    Leaving work

--- app/javascript/channels/resource_channel.js
+++ app/javascript/channels/resource_channel.js
@@ -22,10 +22,47 @@ consumer.subscriptions.create("ResourceChannel", {
 });
 
 let submit_messages;
+let resource_id;
 
 $(document).on('turbolinks:load', function () {
+  // TODO look for resource id from page and subscribe to the channel of that resource
   submit_messages()
+
+  consumer.subscriptions.create("ResourceChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("Connected to the Resource ")
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log(data)
+      $('#resource_messages').append('<div class="message"> <strong>' + data.user + '</strong>: ' + data.content + '</div>')
+      // let message = '<a class="list-group-item list-group-item-action d-flex align-items-center justify-content-between" href="/messages/15"> <div> <strong>alice</strong>' +
+      //  '<small class="text-muted">' + data. '</small> next i restarted the server for any changes </div> <span class="btn btn-sm btn-outline-secondary"> <i class="fas fa-chevron-right fa-fw"></i> </span> </a> </div>'
+      console.log("Recieving:")
+      console.log(data.content)
+    }
+  });
+
+  // App.cable.subscriptions.create(
+  //   {
+  //     channel: "ResourceChannel",
+  //     resource: resource_id()
+  //   },
+  //   {
+  //     received: "Well here's a string"
+  //   }
+  // )
 })
+
+resource_id = function () {
+  $('#resource_messages').attr('data-resource-id')
+}
 
 submit_messages = function () {
   $('#message_body').on('keydown', function (event) {
